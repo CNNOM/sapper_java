d367efbd46b95718eb5bb202e9c0b2ed9035a216
Алексей Зенков <zenkov.m700@mail.ru>
2024-10-29 10:52:13 +0300

    Добавлены интерфейсы и абстрактный класс

--- src/main/java/com/example/sapper/HelloApplication.java
+++ src/main/java/com/example/sapper/HelloApplication.java
@@ -4,9 +4,16 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
+    private GameLogic gameLogic;
+
+    public HelloApplication() {
+        this.gameLogic = new Game();
+    }
+
     @Override
     public void start(Stage stage) {
-        DifficultyDialog.show(stage);
+        RulesDialog rulesDialog = new RulesDialog();
+        rulesDialog.show(stage, gameLogic);
     }
 
     public static void main(String[] args) {
